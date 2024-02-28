80e088953473dd0f3edbe1e9ffe9b988e92668de
Lost-Fly <114507453+Lost-Fly@users.noreply.github.com>
2024-02-28 21:41:21 +0500

    spring initialize

--- server/src/main/java/com/aqwsxlostfly/packandgo/packandgo/PackAndGoApplication.java
+++ server/src/main/java/com/aqwsxlostfly/packandgo/packandgo/PackAndGoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PackandgoApplication {
-
+public class PackAndGoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PackandgoApplication.class, args);
+		SpringApplication.run(PackAndGoApplication.class, args);
 	}
 
 }
