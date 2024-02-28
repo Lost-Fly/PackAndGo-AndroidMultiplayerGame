fd3893ef9e5ad49d155e81a8783d0b0e1633e3f5
Lost-Fly <114507453+Lost-Fly@users.noreply.github.com>
2024-02-28 21:29:21 +0500

    spring create app

---
+++ server/src/main/java/com/aqwsxlostfly/packandgo/packandgo/PackandgoApplication.java
@@ -0,0 +1,13 @@
+package com.aqwsxlostfly.packandgo.packandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackandgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackandgoApplication.class, args);
+	}
+
+}
